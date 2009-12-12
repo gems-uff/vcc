59a285a8dc00e46d22a95d862f14547632633961
thiagooliveira <thiagooliveira@8ce5da43-60f7-4d79-8730-71ec9b2a5ebc>
2009-12-12 23:37:47 +0000

    Imprimindo o suporte no final do arquivo result_plwap.data

--- PLWAP/plwap.cpp
+++ PLWAP/plwap.cpp
@@ -317,8 +317,13 @@ Called in Parameters:
                         tempPattern.push(pnt->event);
                         queue<int> otherPattern = tempPattern;
 
+<<<<<<< .mine
+                        //char buffer[100];
+                        //sprintf(buffer, "%f", (float)count/(float)seqNumber);
+=======
                         char buffer[100];
                         sprintf(buffer, "%f", (float)count/(float)seqNumber);
+>>>>>>> .r20
                         ofstream result("result_PLWAP.data", ios::app);
                         FILE *txt = fopen("result_PLWAP.data", "a+");
                         fprintf(txt, "%s\n", buffer);
@@ -329,6 +334,7 @@ Called in Parameters:
                                 result<<tempPattern.front()<<";";
                                 tempPattern.pop();
                         }
+                        result<<" #"<<(float)count/(float)seqNumber;
                         result<<endl;
 
                         if ( totalSon >= frequency)
