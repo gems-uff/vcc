49d92e7a6be4e7d52c2de9b37690dee14b0ba3dd
luiznunes <luiznunes@8ce5da43-60f7-4d79-8730-71ec9b2a5ebc>
2010-03-25 00:46:44 +0000

    Classe para retornar o resultado da mineração. incompleto.

---
+++ src/br/uff/projetofinal/Parser.java
@@ -0,0 +1,110 @@
+package br.uff.projetofinal;
+
+import java.io.IOException;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+
+import de.vogella.jdt.astsimple.handler.MethodInvocationVisitor;
+import de.vogella.jdt.astsimple.handler.MethodVisitor;
+
+public class Parser extends AbstractHandler
+{
+    private void parseMethod(String projectName, String packageName, String className, String methodSignature)
+    {
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        IWorkspaceRoot root = workspace.getRoot();
+        // Get specified project in the workspace
+        IProject project = root.getProject(projectName);
+        IPackageFragmentRoot iPackage = JavaCore.create(project).getPackageFragmentRoot(packageName);
+        IPackageFragment iPackageFragment = iPackage.getPackageFragment(packageName);
+        ICompilationUnit comp = iPackageFragment.getCompilationUnit(className);
+        CompilationUnit parse = parse(comp);
+        MethodVisitor visitor = new MethodVisitor();
+
+        parse.accept(visitor);
+
+        try
+        {
+            for (MethodDeclaration method : visitor.getMethods())
+            {
+                if (getMethodSignature(method.resolveBinding()).equals(methodSignature))
+                {
+                    MethodInvocationVisitor visitor2 = new MethodInvocationVisitor();
+                    Block methodBody = method.getBody();
+                    if (methodBody == null)
+                        break;
+                    method.getBody().accept(visitor2);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private String getMethodSignature(IMethodBinding methodBinding) throws IOException
+    {
+        methodBinding.getDeclaringClass().getBinaryName();
+
+        String methodName = methodBinding.getName();
+
+        String parametersTypes = getParametersType(methodBinding.getParameterTypes());
+
+        return methodName + "(" + parametersTypes + ")";
+    }
+
+    private String getParametersType(ITypeBinding[] parametersType) throws IOException
+    {
+        String parametersTypes = "";
+        for (int i = 0; i < parametersType.length; i++)
+        {
+            if (i != 0)
+                parametersTypes += ", ";
+            parametersTypes += parametersType[i].getBinaryName();
+        }
+
+        return parametersTypes;
+    }
+
+    /**
+     * Reads a ICompilationUnit and creates the AST DOM for manipulating the
+     * Java source file
+     * 
+     * @param unit
+     * @return
+     */
+
+    private static CompilationUnit parse(ICompilationUnit unit)
+    {
+        ASTParser parser = ASTParser.newParser(AST.JLS3);
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+        parser.setSource(unit);
+        parser.setResolveBindings(true);
+        return (CompilationUnit) parser.createAST(null); // parse
+    }
+
+    @Override
+    public Object execute(ExecutionEvent arg0) throws ExecutionException
+    {
+        parseMethod("JFreeChart", "org.jfree.chart", "ChartFactory.java", "createPieChart");
+        return null;
+    }
+}
