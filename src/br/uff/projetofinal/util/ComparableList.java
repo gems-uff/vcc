f3d6e0312eefa1f60b2d1107c95996e41378216c
luiznunes <luiznunes@8ce5da43-60f7-4d79-8730-71ec9b2a5ebc>
2010-04-19 01:05:00 +0000

    Lista que ordena os elementos por tamanho. Importante para a poda.

---
+++ src/br/uff/projetofinal/util/ComparableList.java
@@ -0,0 +1,23 @@
+package br.uff.projetofinal.util;
+
+import java.util.ArrayList;
+
+public class ComparableList<E> extends ArrayList<E> implements Comparable<ComparableList<E>>
+{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public int compareTo(ComparableList<E> o)
+    {
+        if(this.size() > o.size())
+            return 1;
+        else
+            if(this.size() < o.size())
+                return -1;
+        return 0;
+    }
+    
+}
