ae1d878f992d029f3a98b9b82f456f371db2d2ac
luizlns <luizlns@8ce5da43-60f7-4d79-8730-71ec9b2a5ebc>
2015-01-20 12:21:03 +0000

    Corrigindo a ordem de leitura das chamadas de metodos

--- src/br/uff/vcc/plugin/visitors/MethodInvocationVisitor.java
+++ src/br/uff/vcc/plugin/visitors/MethodInvocationVisitor.java
@@ -3,7 +3,9 @@ package br.uff.vcc.plugin.visitors;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 
 public class MethodInvocationVisitor extends ASTVisitor {
@@ -11,7 +13,23 @@ public class MethodInvocationVisitor extends ASTVisitor {
 
 	@Override
 	public boolean visit(MethodInvocation node) {
-		methods.add(node);
+		boolean isCascadeCall = false;
+		if(node!=null && node.getParent() != null){
+			ASTNode parent = node.getParent();
+			while(!(parent instanceof MethodInvocation) && !(parent instanceof Block) && parent != null){
+				parent = parent.getParent();
+			}
+			if(parent != null){
+				if(parent instanceof MethodInvocation){
+					methods.add(methods.indexOf(parent), node);
+					isCascadeCall = true;
+				}
+			}
+		}
+		
+		if(!isCascadeCall){
+			methods.add(node);
+		}
 		return super.visit(node);
 	}
 
